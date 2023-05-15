b62bfae423c3b67b4884be9a30b043170c808692
ihor-shostak-thg <131873753+ihor-shostak-thg@users.noreply.github.com>
2023-05-15 09:35:59 +0000

    test check

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if (password.length() < 6) return false;
+        
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasNumber = false;
+
+        for (char c : password.toCharArray()) {
+            if (c >= 48 && c <= 57) hasNumber = true;
+            if (c >= 97 && c <= 122) hasLower = true;
+            if (c >= 65 && c <= 90) hasUpper = true;
+        }
+         
+        return hasUpper && hasLower && hasNumber;
     }
 
     public static void main(String[] args) {
